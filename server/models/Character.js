b33b89369ea2480d68cef9dcb174f76954ae1c58
pbarkley <42686983+pbarkley@users.noreply.github.com>
2022-08-31 16:26:59 -0500

    finish character schema

---
+++ server/models/Character.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose')
+
+const characterSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    stats: [
+        {
+            hp: {
+                type: Number,
+                required: true
+            },
+            damage: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    credits: {
+        type: Number,
+        required: true
+    },
+    items: [
+        {
+            itemName: {
+                type: String
+            }
+        }
+    ],
+    abilities: [
+        {
+            abilityName: {
+                type: String
+            }
+        }
+    ]
+    store: {
+        // not sure what goes here
+    }
+});
+
+const Character = model('Character', characterSchema);
+
+module.exports = Character;
