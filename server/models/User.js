8605c747dda73265a99251ae15ea0885b43437aa
pbarkley_desk <rpbarkley99@gmail.com>
2022-09-01 19:41:07 -0500

    create user seed

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,8 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
