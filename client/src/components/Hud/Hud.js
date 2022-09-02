8df6b76b2c6d5ae3ea7291a71228bd37fb6afa9d
Liam McCarthy <ltmccarthy9@gmail.com>
2022-09-02 17:26:53 -0500

    hud component/slap icon

---
+++ client/src/components/Hud/Hud.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default () => {
+    return(
+        <div>
+            //Slap ability icon
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 25 25" shape-rendering="crispEdges">
+                <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
+                <path stroke="#dde4f2" d="M0 0h1M4 0h4M15 0h3M0 1h3M7 1h3M17 1h2M2 2h3M9 2h3M18 2h3M4 3h2M11 3h3M20 3h2M0 4h2M1 5h3M3 6h3M0 10h1M0 11h2M1 12h2M2 13h2" />
+                <path stroke="#cbdbfc" d="M1 0h3M8 0h7M18 0h7M3 1h4M10 1h7M19 1h6M0 2h2M5 2h4M12 2h6M21 2h4M0 3h4M6 3h5M14 3h1M17 3h3M22 3h3M2 4h12M21 4h4M0 5h1M4 5h4M11 5h2M21 5h4M0 6h3M6 6h1M11 6h1M21 6h4M0 7h7M22 7h3M0 8h6M22 8h3M0 9h6M23 9h2M1 10h4M23 10h2M2 11h3M23 11h2M0 12h1M3 12h1M23 12h2M0 13h2M23 13h2M0 14h4M22 14h3M0 15h4M21 15h4M0 16h4M20 16h5M0 17h3M19 17h6M0 18h2M18 18h7M0 19h1M17 19h8M16 20h9M15 21h10M13 22h12M10 23h15M9 24h16" />
+                <path stroke="#eec39a" d="M15 3h2M14 4h4M19 4h2M8 5h3M13 5h4M18 5h3M7 6h4M12 6h4M17 6h3M7 7h3M11 7h4M16 7h3M20 7h1M6 8h4M11 8h3M15 8h3M19 8h3M6 9h3M10 9h3M14 9h3M18 9h4M5 10h3M9 10h3M13 10h3M17 10h4M22 10h1M5 11h10M16 11h4M21 11h2M4 12h10M15 12h4M20 12h3M4 13h14M19 13h4M4 14h13M18 14h4M4 15h17M4 16h16M3 17h16M2 18h16M1 19h16M0 20h16M0 21h15M0 22h13M0 23h10M0 24h9" />
+                <path stroke="#d9a066" d="M18 4h1M17 5h1M16 6h1M20 6h1M15 7h1M19 7h1M14 8h1M18 8h1M13 9h1M17 9h1M22 9h1M12 10h1M16 10h1M21 10h1M15 11h1M20 11h1M14 12h1M19 12h1M18 13h1M17 14h1" />
+                <path stroke="#cd9b68" d="M10 7h1M10 8h1M9 9h1M8 10h1" />
+                <path stroke="#e9b683" d="M21 7h1" />
+            </svg>
+            </div>
+            <div>
+                // nap heal ability icon
+            </div>
+        </div>
+    )
+}
