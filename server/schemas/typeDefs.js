dee9aaac6e544e459814128851ec89d9bb933966
Jondo <chet9880@gmail.com>
2022-09-01 21:22:03 -0400

    added user typedef

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,14 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String!
+        password: String!
+        character: Character
+    }
+
+    
+`
+
+module.exports = typeDefs
