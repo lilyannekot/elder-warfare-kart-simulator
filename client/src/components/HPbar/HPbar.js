ec12378f8046bed0a7f87810d15547446cc3a251
Liam McCarthy <ltmccarthy9@gmail.com>
2022-09-01 19:22:52 -0500

    HPbar component initialization

---
+++ client/src/components/HPbar/HPbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 44 9" shape-rendering="crispEdges">
+            <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
+            <path stroke="#000000" d="M2 0h40M1 1h42M0 2h2M42 2h2M0 3h2M42 3h2M0 4h2M42 4h2M0 5h2M42 5h2M0 6h2M42 6h2M1 7h42M2 8h40" />
+            <path stroke="#cbdbfc" d="M2 2h40M2 3h1M41 3h1M2 4h1M41 4h1M2 5h1M41 5h1M2 6h40" />
+            <path stroke="#222034" d="M3 3h38M3 4h38M3 5h38" />
+        </svg>
+    );
+}
