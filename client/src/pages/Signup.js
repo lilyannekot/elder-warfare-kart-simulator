f5bfe68d61e1bd902af18632a840a5f897c08cdc
lilys tacos <lilyannek@gmail.com>
2022-09-02 10:30:20 -0700

    html to be returned on signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -8,6 +8,7 @@ const Signup = () => {
   const [formState, setFormState] = useState({
     username: "",
     password: "",
+    characterName: "",
   });
   const [addUser, { error, data }] = useMutation(ADD_USER);
 
@@ -35,4 +36,57 @@ const Signup = () => {
       console.error(error);
     }
   };
+
+  return (
+    <main className="flex-row justify-center">
+      <div>
+        <div className="card">
+          <h4 className="text-light">Signup</h4>
+          <div className="card-body">
+            {data ? (
+              <p>
+                <Link to="/instructions">Head to Instructions Page</Link>
+              </p>
+            ) : (
+              <form onSubmit={formChanges}>
+                <input
+                  placeholder="Username"
+                  name="username"
+                  type="username"
+                  value={formState.username}
+                  onChange={formChanges}
+                />
+                <input
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onchange={formChanges}
+                />
+                <input
+                  placeholder="Character Name"
+                  name="characterName"
+                  value={formState.characterName}
+                  onChange={formChanges}
+                />
+                <button
+                  className="btn btn-danger"
+                  style={{ cursor: "pointer" }}
+                  type="submit"
+                >
+                  Signup
+                </button>
+              </form>
+            )}
+
+            {error && (
+              <div className="bg-danger text-white">{error.message}</div>
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
+
+export default Signup;
