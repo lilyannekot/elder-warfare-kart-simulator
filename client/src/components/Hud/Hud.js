fbbd4af8e3d05397b74dd576b47bb910a9ae1ef8
Liam McCarthy <ltmccarthy9@gmail.com>
2022-09-03 16:10:32 -0500

    hud component nap/runaway

--- client/src/components/Hud/Hud.js
+++ client/src/components/Hud/Hud.js
@@ -16,7 +16,37 @@ export default () => {
             </svg>
             </div>
             <div>
-                // nap heal ability icon
+            // nap heal ability icon*
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 25 25" shape-rendering="crispEdges">
+                <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
+                <path stroke="#222034" d="M0 0h25M0 1h21M22 1h3M0 2h5M6 2h14M23 2h2M0 3h4M7 3h14M22 3h3M0 4h5M6 4h19M0 5h25M0 6h16M17 6h8M0 7h15M18 7h7M0 8h7M9 8h7M17 8h8M0 9h5M10 9h15M0 10h5M10 10h1M19 10h1M21 10h4M0 11h5M19 11h1M21 11h4M0 12h5M21 12h4M0 13h5M21 13h4M0 14h5M21 14h4M0 15h5M21 15h4M0 16h5M6 16h14M21 16h4M0 17h2M3 17h2M6 17h14M21 17h4M0 18h1M4 18h21M0 19h2M3 19h16M20 19h5M0 20h18M21 20h4M0 21h19M20 21h5M0 22h25M0 23h25M0 24h25" />
+                <path stroke="#6abe30" d="M21 1h1M5 2h1M20 2h3M4 3h3M21 3h1M5 4h1M16 6h1M15 7h3M16 8h1" />
+                <path stroke="#eec39a" d="M7 8h2M8 9h2M8 10h2M20 10h1M9 11h1M20 11h1M19 12h2" />
+                <path stroke="#d9a066" d="M5 9h1M5 10h1M5 11h1M5 12h1M5 13h1M5 14h1M5 15h1M5 16h1M20 16h1M5 17h1M20 17h1" />
+                <path stroke="#ffffff" d="M6 9h2M6 10h2M6 11h3M6 12h4M7 13h2" />
+                <path stroke="#847e87" d="M11 10h8M10 11h9M10 12h9M9 13h10" />
+                <path stroke="#639bff" d="M6 13h1M19 13h2M6 14h15M6 15h15" />
+                <path stroke="#99e550" d="M2 17h1M1 18h3M2 19h1M19 19h1M18 20h3M19 21h1" />
+            </svg>
+            </div>
+            <div>
+                //run away
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 25 25" shape-rendering="crispEdges">
+                <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
+                <path stroke="#639bff" d="M0 0h5M16 0h9M0 1h4M15 1h10M0 2h5M13 2h12M0 3h4M13 3h12M0 4h5M13 4h12M0 5h5M7 5h1M12 5h13M0 6h9M10 6h4M19 6h6M0 7h9M19 7h6M1 8h8M11 8h4M19 8h6M4 9h4M12 9h5M18 9h7M0 10h1M2 10h1M5 10h3M13 10h4M19 10h6M0 11h1M2 11h6M13 11h4M20 11h5M1 12h2M4 12h4M13 12h4M21 12h4M0 13h2M14 13h3M22 13h3M1 14h2M14 14h2M23 14h2M23 15h2M0 16h2M23 16h2M0 17h3M23 17h2M0 18h4M10 18h4M20 18h5M0 19h4M10 19h4M20 19h5" />
+                <path stroke="#ffffff" d="M5 0h11M4 1h11M5 2h6M4 3h3M8 3h1M5 4h3M5 5h2" />
+                <path stroke="#eec39a" d="M11 2h2M7 3h1M9 3h2M12 3h1M8 4h5M8 5h4M9 6h1M14 6h2M10 7h3M14 7h1" />
+                <path stroke="#5fcde4" d="M11 3h1" />
+                <path stroke="#222034" d="M16 6h3M13 7h1M15 7h1M17 7h2M15 8h4M8 9h1M17 9h1M8 10h1M17 10h2M8 11h4M17 11h3M8 12h4M17 12h4M3 13h9M17 13h5M3 14h11M16 14h7M3 15h18M3 16h20M3 17h2M9 17h6M19 17h4M0 20h5M9 20h6M19 20h6M0 21h25M3 22h4M10 22h3M16 22h3M22 22h3M0 23h25M0 24h25" />
+                <path stroke="#df7126" d="M9 7h1M9 8h2M9 9h3" />
+                <path stroke="#d95763" d="M16 7h1" />
+                <path stroke="#cbdbfc" d="M0 8h1M0 9h4M1 10h1M3 10h2M1 11h1M0 12h1M3 12h1M2 13h1M0 14h1M0 15h3M2 16h1" />
+                <path stroke="#306082" d="M9 10h4M12 11h1M12 12h1" />
+                <path stroke="#663931" d="M12 13h2" />
+                <path stroke="#fbf236" d="M21 15h2M0 22h3M7 22h3M13 22h3M19 22h3" />
+                <path stroke="#000000" d="M5 17h4M15 17h4M4 18h2M8 18h2M14 18h2M18 18h2M4 19h2M8 19h2M14 19h2M18 19h2M5 20h4M15 20h4" />
+                <path stroke="#595652" d="M6 18h2M16 18h2M6 19h2M16 19h2" />
+            </svg>
             </div>
         </div>
     )
