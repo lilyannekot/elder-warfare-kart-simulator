1fac1c9c3da47ad8cf7afa51381f9aec238790fd
lilys tacos <lilyannek@gmail.com>
2022-09-02 17:51:54 -0700

    requiring mongoose on models

--- server/models/Character.js
+++ server/models/Character.js
@@ -1,53 +1,54 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
 
 const characterSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    hp: {
-        type: Number,
-        required: true
-    },
-    damage: {
-        type: Number,
-        required: true
-    },
-    hitChance: {
-        type: Number,
-        required: true
-    },
-    dodge: {
-        type: Number,
-        required: true
-    }
-    credits: {
-        type: Number,
-        required: true
-    },
-    items: [
-        {
-            itemId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Item'
-            }
-        }
-    ],
-    abilities: [
-        {
-            abilityId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Ability'
-            }
-        }
-    ],
-    user: {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  hp: {
+    type: Number,
+    required: true,
+  },
+  damage: {
+    type: Number,
+    required: true,
+  },
+  hitChance: {
+    type: Number,
+    required: true,
+  },
+  dodge: {
+    type: Number,
+    required: true,
+  },
+  credits: {
+    type: Number,
+    required: true,
+  },
+  items: [
+    {
+      itemId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: "Item",
+      },
     },
+  ],
+  abilities: [
+    {
+      abilityId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ability",
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
-const Character = model('Character', characterSchema);
+const Character = model("Character", characterSchema);
 
 module.exports = Character;
