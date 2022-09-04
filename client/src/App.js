27e8b5917ddc75f5f47cd13b10034e280ec5369d
lilys tacos <lilyannek@gmail.com>
2022-09-04 14:59:54 -0700

    Moved HP logic to battle scene component and refactored App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React from "react";
+// import logo from "./logo.svg";
 import "./App.css";
 import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
@@ -8,9 +8,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/">
-          <Login />
-        </Route>
+        <Route path="/" element={<Login />}></Route>
       </Routes>
     </div>
   );
