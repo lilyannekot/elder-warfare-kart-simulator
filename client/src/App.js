4ca9e4ef1a23b517892a43fed4730354ce2d2934
lilys tacos <lilyannek@gmail.com>
2022-09-04 14:56:15 -0700

    added login mutation logic in mutations.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import HPbar from './components/BattlePage/HPbar/HPbar'
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <HPbar
-        currentHP={50}
-        maxHP={100}
-      />
+    <div>
+      <Routes>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ client/src/components/BattlePage/BattleScene/index.js
@@ -0,0 +1,7 @@
+import HPbar from "../HPbar/HPbar";
+
+return (
+  <div className="App">
+    <HPbar currentHP={50} maxHP={100} />
+  </div>
+);
