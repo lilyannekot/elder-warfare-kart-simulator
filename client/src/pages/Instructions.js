435a5c4e6b5b1ce5139ddf2b8b4d1cde84ba48a3
Liam McCarthy <ltmccarthy9@gmail.com>
2022-09-04 15:25:17 -0500

    instructions page

--- client/src/pages/Instructions.js
+++ client/src/pages/Instructions.js
@@ -5,9 +5,10 @@ function Instructions() {
         <div>
             <h1>Welcome to Elder Warfare Kart Simulator!</h1>
             <h3>Here is all you need to know to play:</h3>
-            <ul>
-                <li>don't</li>
-            </ul>
+            <h4>The game is centered around turn based combat and purhasing items to boost your stats.</h4>
+            <h4>You have HP and attack stats, your attack stat modifies the damage your abilities do.</h4>
+            <h4>After combat, you are taken to a store page where you can choose to buy items for the next battle</h4>
+            <h4>If you lose, your account is deleted.</h4>
         </div>
     )
 }
