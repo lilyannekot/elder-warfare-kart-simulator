5e6ab3e8eb6338ec126c0ccc812a4235205dbc2a
lilys tacos <lilyannek@gmail.com>
2022-09-04 12:00:58 -0700

    decode the token in order to get its expiration time that was set by the server. If the expiration time is less than the current time, the token is expired and true is returned

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,14 +1,19 @@
 import decode from "jwt-decode";
 
-class AuthService{
-    loggedIn() {
-        const token = this.getToken();
-        // If there's a token and it hasn't expired yet, return true. Else, return false
-        return token && !this.isTokenExpired(token) ? true : false;
-    }
+class AuthService {
+  loggedIn() {
+    const token = this.getToken();
+    // If there's a token and it hasn't expired yet, return true. Else, return false
+    return token && !this.isTokenExpired(token) ? true : false;
+  }
 
-    isTokenExpired(token) {
-        // Decode the token in order to get its expiration time that was set by the server
-        const decoded = decode(token);
+  isTokenExpired(token) {
+    // Decode the token in order to get its expiration time that was set by the server
+    const decoded = decode(token);
+
+    if (decoded.exp < Date.now() / 1000) {
+      localStorage.removeItem("id_token");
+      return true;
     }
+  }
 }
