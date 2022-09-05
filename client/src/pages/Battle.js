4a865bb81d7c756a4f15821723e504b67a8447f2
lilys tacos <lilyannek@gmail.com>
2022-09-04 21:59:53 -0700

    added go back to login button with useNavigate method to instructions page

--- client/src/pages/Battle.js
+++ client/src/pages/Battle.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useMutation } from "@apollo/client";
+// import { Link } from "react-router-dom";
+// import { useMutation } from "@apollo/client";
 
 const Battle = (props) => {
     // const enemy = {hp: 100+(wins * 5), attack: 1 +(wins * 5)}
     // const [HP, setHP] = useState(100 + itemHP);
     // const [enemyHP, setEnemyHP] = useState(enemy.hp)
-    
 
  
     return (
-        
+        <div>
+            <h1>Fight!</h1>
+        </div>
     );
 };
+
+export default Battle;
