f7e99a217726057288c5e384861fbe0f12d85e17
Liam McCarthy <ltmccarthy9@gmail.com>
2022-09-04 19:03:39 -0500

    instructions

--- client/src/pages/Battle.js
+++ client/src/pages/Battle.js
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 
 const Battle = (props) => {
+    // const enemy = {hp: 100+(wins * 5), attack: 1 +(wins * 5)}
+    // const [HP, setHP] = useState(100 + itemHP);
+    // const [enemyHP, setEnemyHP] = useState(enemy.hp)
     
+
+ 
     return (
         
     );
