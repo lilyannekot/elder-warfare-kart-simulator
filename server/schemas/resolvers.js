2e78790a3c9c33f81f9bd87172e2ee3ae6cc23f2
Liam McCarthy <ltmccarthy9@gmail.com>
2022-09-04 19:42:55 -0500

    error messages with graphql typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { Character, StoreFront, User } = require("./models");
+const { Character, StoreFront, User } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 
